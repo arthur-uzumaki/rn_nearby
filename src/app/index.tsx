bfdced118d6ffc461cf996ca388b8e2f80d6805e
arthur-uzumaki <arthursousarabelo@gmail.com>
2024-12-10 15:38:42 -0300

    feat: add  component button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,17 @@
+import { Button } from '@/components/button'
 import { Header } from '@/components/header'
 import { Steps } from '@/components/steps'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 export default function Home() {
   return (
     <View className="flex-1 gap-10 p-10 ">
       <Header />
 
       <Steps />
+
+      <Button>
+        <Button.Title>Começa</Button.Title>
+      </Button>
     </View>
   )
 }
